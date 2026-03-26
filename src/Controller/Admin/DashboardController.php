010fd54be5d9209db91ac5aab5b1a9af09fc7abb
bakewizard <207716455+bakewizard@users.noreply.github.com>
2026-03-27 01:01:14 +0200

    fix

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -3,7 +3,6 @@ declare(strict_types=1);
 
 namespace Pages\Controller\Admin;
 
-use App\Attribute\Resource;
 use App\Controller\Admin\AppController;
 
 /**
@@ -20,7 +19,6 @@ class DashboardController extends AppController
      *
      * @return void
      */
-    #[Resource(label: 'Pages dashboard')]
     public function index()
     {
         // Plugin dashboard logic goes here
